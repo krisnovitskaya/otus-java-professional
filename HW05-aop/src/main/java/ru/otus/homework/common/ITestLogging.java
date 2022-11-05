1891e1ac297e1aa827b0f6cbcd43dc87aceeffa1
krisnovitskaya <voitage@yandex.ru>
2022-11-05 10:11:41 +0300

    homework05-aop by proxy

---
+++ HW05-aop/src/main/java/ru/otus/homework/common/ITestLogging.java
@@ -0,0 +1,12 @@
+package ru.otus.homework.common;
+
+
+public interface ITestLogging {
+
+    void calculation();
+    void calculation(int param1);
+
+    void calculation(int param1, int param2);
+
+    void calculation(int param1, int param2, String param3);
+}
---
+++ HW05-aop/src/main/java/ru/otus/homework/proxies/ProxyCreator.java
@@ -0,0 +1,59 @@
+package ru.otus.homework.proxies;
+
+import ru.otus.homework.annotation.Log;
+import ru.otus.homework.common.ITestLogging;
+
+import java.lang.reflect.*;
+import java.util.*;
+
+public class ProxyCreator {
+
+    private ProxyCreator() {
+    }
+
+    public static  ITestLogging createProxyClass(Class<? extends ITestLogging> clazz) throws Exception {
+        return (ITestLogging) Proxy.newProxyInstance(ProxyCreator.class.getClassLoader(),
+                new Class<?>[]{ITestLogging.class}, new LogInvocationHandler(clazz));
+    }
+
+    static class LogInvocationHandler implements InvocationHandler {
+        private final ITestLogging proxiedClass;
+        private final Set<MethodInfo> loggingMethods = new HashSet<>();
+
+        LogInvocationHandler(Class<? extends ITestLogging> clazz) throws Exception {
+            Constructor<? extends ITestLogging> constructor = clazz.getConstructor();
+            this.proxiedClass = constructor.newInstance();
+
+            Method[] declaredMethods = clazz.getDeclaredMethods();
+            for (Method declaredMethod : declaredMethods) {
+                if(declaredMethod.isAnnotationPresent(Log.class)){
+                    loggingMethods.add(new MethodInfo(declaredMethod.getName(), Arrays.stream(declaredMethod.getParameterTypes()).toList()));
+                }
+            }
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException {
+
+            MethodInfo methodInfo = new MethodInfo(method.getName(), Arrays.stream(method.getParameterTypes()).toList());
+            if(loggingMethods.contains(methodInfo)){
+                logging(methodInfo, args);
+            }
+            return method.invoke(proxiedClass, args);
+        }
+
+        @Override
+        public String toString() {
+            return "LogInvocationHandler{" +
+                    "proxiedClass=" + proxiedClass +
+                    '}';
+        }
+
+        private void logging(MethodInfo methodInfo, Object[] args){
+            System.out.printf("executed method: %s, params: %s%n", methodInfo.name,
+                    Optional.ofNullable(args).map(arg -> Arrays.stream(arg).toList().toString()).orElse("[]"));
+        }
+    }
+
+    record MethodInfo(String name, List<Class<?>> parameters){}
+}
