60934949242c32f2a2efcb3541ac3557e28193a9
krisnovitskaya <voitage@yandex.ru>
2022-12-11 17:43:00 +0300

    hw08 json parser

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,29 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
+    private final String fileName;
+
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        //формирует результирующий json и сохраняет его в файл
+        var gson = new Gson();
+        var jsonText = gson.toJson(data);
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName))){
+            bos.write(jsonText.getBytes());
+            bos.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -4,12 +4,16 @@ import ru.otus.model.Measurement;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        //группирует выходящий список по name, при этом суммирует поля value
-        return null;
+        Map<String, Double> map = new TreeMap<>();
+        for (Measurement datum : data) {
+            map.merge(datum.getName(), datum.getValue(), Double::sum);
+        }
+        return map;
     }
 }
