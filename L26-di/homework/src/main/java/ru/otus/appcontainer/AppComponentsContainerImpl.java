1b192739e96b2aeb02ebf8c618a54fe21b40bbb4
krisnovitskaya <voitage@yandex.ru>
2023-01-08 16:30:24 +0300

    hw13 basic done

--- L26-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L26-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,8 +1,11 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -10,14 +13,43 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws Exception {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
+    private void processConfig(Class<?> configClass) throws Exception {
         checkConfigClass(configClass);
         // You code here...
+        Object configClassInstance = configClass.getConstructor().newInstance();
+
+        List<Method> methods = Arrays.stream(configClass.getMethods()).filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order())).toList();
+
+        String componentName;
+        Object component;
+        Parameter[] parameters;
+        for (Method method : methods) {
+            parameters = method.getParameters();
+            componentName = method.getAnnotation(AppComponent.class).name();
+            if (appComponentsByName.containsKey(componentName)) {
+                throw new Exception("duplicate component name: " + componentName);
+            }
+
+            if (parameters.length == 0) {
+                component = method.invoke(configClassInstance);
+            } else {
+                Object[] args = new Object[parameters.length];
+                for (int i = 0; i < args.length; i++) {
+                    args[i] = getAppComponent(parameters[i].getType());
+                }
+                component = method.invoke(configClassInstance, args);
+            }
+
+            appComponentsByName.put(componentName, component);
+            appComponents.add(component);
+        }
     }
+
 
     private void checkConfigClass(Class<?> configClass) {
         if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
@@ -27,11 +59,11 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        return (C) appComponents.stream().filter(component -> componentClass.isAssignableFrom(component.getClass())).findAny().orElse(null);
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
