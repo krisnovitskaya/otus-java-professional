aeb7e684cde58d44a0ad8d8fb8c38d81c706865f
krisnovitskaya <voitage@yandex.ru>
2023-01-22 13:36:47 +0300

    refactor after review

--- L26-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L26-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -15,7 +15,7 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> ... initialConfigClasses) throws Exception {
+    public AppComponentsContainerImpl(Class<?>... initialConfigClasses) throws Exception {
         for (Class<?> initialConfigClass : initialConfigClasses) {
             checkConfigClass(initialConfigClass);
         }
@@ -29,50 +29,12 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         this(new Reflections(packageName, Scanners.TypesAnnotated).getTypesAnnotatedWith(AppComponentsContainerConfig.class).toArray(new Class<?>[0]));
     }
 
-    private void processConfig(Class<?> configClass) throws Exception {
-        Object configClassInstance = configClass.getConstructor().newInstance();
-
-        List<Method> methods = Arrays.stream(configClass.getMethods()).filter(method -> method.isAnnotationPresent(AppComponent.class))
-                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order())).toList();
-
-        String componentName;
-        Object component;
-        Parameter[] parameters;
-        for (Method method : methods) {
-            parameters = method.getParameters();
-            componentName = method.getAnnotation(AppComponent.class).name();
-            if (appComponentsByName.containsKey(componentName)) {
-                throw new AppContainerException("duplicate component name: " + componentName);
-            }
-
-            if (parameters.length == 0) {
-                component = method.invoke(configClassInstance);
-            } else {
-                Object[] args = new Object[parameters.length];
-                for (int i = 0; i < args.length; i++) {
-                    args[i] = getAppComponent(parameters[i].getType());
-                }
-                component = method.invoke(configClassInstance, args);
-            }
-
-            appComponentsByName.put(componentName, component);
-            appComponents.add(component);
-        }
-    }
-
-
-    private void checkConfigClass(Class<?> configClass) {
-        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
-            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
-        }
-    }
-
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
         List<Object> components = appComponents.stream().filter(component -> componentClass.isAssignableFrom(component.getClass())).toList();
-        if(components.size() > 1 ){
+        if (components.size() > 1) {
             throw new AppContainerException("getting component is present in container more then one instance");
-        }else if (components.size() == 0){
+        } else if (components.size() == 0) {
             throw new AppContainerException("getting component is absent in container");
         }
         return (C) components.get(0);
@@ -81,5 +43,47 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     @Override
     public <C> C getAppComponent(String componentName) {
         return (C) Optional.ofNullable(appComponentsByName.get(componentName)).orElseThrow(() -> new AppContainerException("getting component is absent in container"));
+    }
+
+    private void processConfig(Class<?> configClass) throws Exception {
+        Object configClassInstance = configClass.getConstructor().newInstance();
+
+        List<Method> methods = Arrays.stream(configClass.getMethods()).filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order())).toList();
+
+        String componentName;
+        Object component;
+        for (Method method : methods) {
+            componentName = getComponentName(method);
+            checkDuplicate(componentName);
+            component = method.invoke(configClassInstance, getArgs(method.getParameters()));
+
+            appComponentsByName.put(componentName, component);
+            appComponents.add(component);
+        }
+    }
+
+    private String getComponentName(Method method) {
+        return method.getAnnotation(AppComponent.class).name();
+    }
+
+    private void checkDuplicate(String componentName) {
+        if (appComponentsByName.containsKey(componentName)) {
+            throw new AppContainerException("duplicate component name: " + componentName);
+        }
+    }
+
+    private Object[] getArgs(Parameter[] parameters) {
+        Object[] args = new Object[parameters.length];
+        for (int i = 0; i < args.length; i++) {
+            args[i] = getAppComponent(parameters[i].getType());
+        }
+        return args;
+    }
+
+    private void checkConfigClass(Class<?> configClass) {
+        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
+            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+        }
     }
 }
