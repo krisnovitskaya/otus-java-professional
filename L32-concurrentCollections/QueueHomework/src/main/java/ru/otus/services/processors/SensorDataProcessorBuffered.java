d8a30fa0a08071d5a14f3700420a25049abac118
krisnovitskaya <voitage@yandex.ru>
2023-02-13 05:58:28 +0300

    homework16

--- L32-concurrentCollections/QueueHomework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ L32-concurrentCollections/QueueHomework/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -2,9 +2,14 @@ package ru.otus.services.processors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.otus.lib.SensorDataBufferedWriter;
 import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 // Этот класс нужно реализовать
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
@@ -13,23 +18,31 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
 
+    private final Set<SensorData> dataBuffer;
+
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.dataBuffer = new ConcurrentSkipListSet<>(Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-    /*
+        dataBuffer.add(data);
         if (dataBuffer.size() >= bufferSize) {
             flush();
         }
-    */
     }
 
     public void flush() {
         try {
-            //writer.writeBufferedData(bufferedData);
+            synchronized (dataBuffer) {
+                if (dataBuffer.size() > 0) {
+                    List<SensorData> sensorData = dataBuffer.stream().toList();
+                    writer.writeBufferedData(sensorData);
+                    dataBuffer.removeAll(sensorData);
+                }
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
